3c9b6ccdadc1b8dc594d493844e3a437e0cae0f3
yevhen-m <yvhn.yvhn@gmail.com>
2015-06-16 18:04:23 +0300

    Ecomap frontend.

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Kv-008.frontend
+
+this script assumes you have npm, node, bower and mysql installed
+```sudo apt-get install nodejs npm```
+and then
+```sudo npm install -g bower```
+```sudo apt-get install mysql```
+mysql creds:
+  user: root
+  password: root
+
+Before launching the script you may need to enter:
+  ```git config --global url."https://".insteadOf git://```
+
+
+Then create deploy.sh script with the next contents:
+```
+mkdir -p /var/tmp/deploy/
+cd /var/tmp/deploy/
+rm -rf KV004/
+git clone https://github.com/ITsvetkoFF/KV004.git
+cd KV004/frontend/
+npm install
+npm install -g grunt-cli
+bower install --allow-root
+grunt distOn
+grunt
+cd ../backend
+npm install
+mysql -u root -proot < EnviromapDB.sql
+nodejs filldb.js
+```
+and then run
+```sudo sh deploy.sh```
+
+then you can run ecomap typing
+```nodejs api.js```
+in the /var/tmp/deploy/KV004/backend/ folder
---
+++ app/controllers/map-load.js
@@ -0,0 +1,168 @@
+define(['./module'], function (controllers) {
+    'use strict';
+    controllers.controller('mapLoadCtrl', ['$scope', 'ProblemService', '$rootScope', 'UserService', '$routeParams', '$route', '$location', 'todayTime', '$timeout', 'windowWidth', function ($scope, ProblemService, $rootScope, UserService, $routeParams, $route, $location, todayTime, $timeout, windowWidth) {
+        $scope.isAdministrator = UserService.isAdministrator;
+        $scope.getWindowDimensions = windowWidth.getWindowDimensions;
+
+        $rootScope.$broadcast('Update', "");
+
+        var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            minZoom: 2,
+            detectRetina: true,
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        });
+        var geoJson = L.geoJson(ukraineBorders, {
+            style: {
+                opacity: 0,
+                fillOpacity: 0
+            }
+        });
+        var latlng = L.latLng(50.00, 32.00);
+
+        L.Map.prototype.panToOffset = function (latlng, paddingL, paddingT, paddingR, paddingB) {
+            var x1 = latlng.lat - 0.01,
+                y1 = latlng.lng - 0.01,
+                x2 = latlng.lat + 0.01,
+                y2 = latlng.lng + 0.01;
+            return this.fitBounds([[x1, y1], [x2, y2]], {
+                maxZoom: 14,
+                paddingTopLeft: [paddingL, paddingT],
+                paddingBottomRight: [paddingR, paddingB]
+            });
+        };
+
+        $rootScope.map = L.map('map-content', {
+            center: latlng,
+            zoom: 7,
+            minZoom: 2,
+            //maxBounds: L.latLngBounds(L.latLng(43, 19), L.latLng(53, 46)),
+            layers: [tiles, geoJson]
+        });
+        $timeout($rootScope.map.invalidateSize.bind($rootScope.map), 200);
+        $rootScope.geoJson = geoJson; //forwarding geoJson object to $rooTscope
+        var markers = L.markerClusterGroup();
+        $scope.data = {};
+        var markerIcon;
+
+        $rootScope.getProblemsAndPlaceMarkers = function () {
+            ProblemService.getAllProblemsFromDb()
+                .success(function (data) {
+                    $scope.data = data;
+                    placeMarkers($scope.data);
+                })
+                .error(function (data, status, headers, config) {
+                    throw error;
+                });
+        };
+        $scope.getProblemsAndPlaceMarkers();
+
+        $scope.locateUser = function () {
+            navigator.geolocation.getCurrentPosition(getUserPosition);
+            var width = $scope.getWindowDimensions();
+
+            function getUserPosition(position) {
+                var mapCenter = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                if (width < 1000) {
+                    $rootScope.map.panToOffset(mapCenter, 0, 90, 0, 0);
+                } else {
+                    $rootScope.map.panToOffset(mapCenter, 0, 0, 600, 0);
+                }
+            }
+        }
+
+        function onMarkerClick(marker) {
+            if (UserService.getSaveChangeStatus()) {
+                $scope.uploadRightSide = false;
+                window.location.href = "#/map";
+                window.location.href = "#/problem/showProblem/" + this._id;
+            }
+
+        }
+
+        function placeMarkers(data) {
+            markers.clearLayers();
+            var newData = userSelectionFilterMarkers(data);
+            angular.forEach(newData, function (problem) {
+                markerIcon = L.icon({
+                    //iconUrl : 'images/markers/'+location['ProblemTypes_Id']+ '.png',
+                    iconUrl: 'images/markers/' + problem.problem_type_id + '.png',
+                    iconAnchor: [25, 79]
+                });
+
+                //var marker = L.marker([location.Latitude, location.Longtitude], {icon: markerIcon});
+                var marker = L.marker([problem.lat, problem.lon], {icon: markerIcon});
+                marker.on('click', onMarkerClick);
+                marker._id = problem['id'];
+                markers.addLayer(marker);
+                $rootScope.map.addLayer(markers);
+            });
+            $rootScope.map.addLayer(markers);
+        };
+
+        function userSelectionFilterMarkers(data) {
+            var tempData = [];
+            for (var i = 0; i < data.length; i++) {
+                var problem = data[i];
+                var problemTypeSelected = isSelected($scope.problemTypes, problem.problem_type_id);
+                //var problemStatusSelected = isSelected($scope.problemStatuses, problem.status);
+                var problemStatusSelected = true;
+                if (problemTypeSelected && problemStatusSelected) {
+                    tempData.push(problem);
+                }
+            }
+            //var newData = dateRange(tempData);
+            return tempData;
+        };
+
+        function isSelected(filtersArray, ProblemTypes_Id) {
+            for (var i = 0; i < filtersArray.length; i++) {
+                if (filtersArray[i].id == ProblemTypes_Id) {
+                    return filtersArray[i].selected;
+                }
+            }
+        }
+
+        function dateRange(data) {
+            var dateFrom = Date.parse(todayTime.formDataDt);
+            var dateTill = Date.parse(todayTime.formDataDtSecond);
+            var tempData = [];
+            for (var i = 0; i < data.length; i++) {
+                var problem = data[i];
+                var locationDate = Date.parse(problem.Date);
+                if (dateFrom < locationDate && locationDate < dateTill) {
+                    tempData.push(problem);
+                }
+            }
+            var newData = tempData;
+            return newData;
+        }
+
+        $scope.problemTypes = [
+            {name: 'problem_type_0', id: 1, selected: true},
+            {name: 'problem_type_1', id: 2, selected: true},
+            {name: 'problem_type_2', id: 3, selected: true},
+            {name: 'problem_type_3', id: 4, selected: true},
+            {name: 'problem_type_4', id: 5, selected: true},
+            {name: 'problem_type_5', id: 6, selected: true},
+            {name: 'problem_type_6', id: 7, selected: true},
+            {name: 'problem_type_7', id: 8, selected: true}
+        ];
+
+        $scope.problemStatuses = [
+            {name: 'SOLVED', selected: true},
+            {name: 'UNSOLVED', ecselected: true}
+        ];
+
+        $scope.placeUserProblemsChecker;
+        $scope.toggleSelection = function () {
+            if ($scope.placeUserProblemsChecker)
+                placeMarkers($scope.dataUserProblems);
+            else
+                placeMarkers($scope.data);
+        };
+
+    }]);
+});
