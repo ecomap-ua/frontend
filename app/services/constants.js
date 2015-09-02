d2ba682bd18e546cb244f2f6575b32f1cf7c7144
itsvetkoff <tsvetkoff@i.ua>
2015-09-02 18:50:33 +0300

    change hardcoded api to constant service

--- app/services/constants.js
+++ app/services/constants.js
@@ -1,9 +1,8 @@
 define(['./module'], function (services) {
     'use strict';
-
+    var API_HOST = 'http://127.0.0.1:8000/';
+    //http://176.36.11.25:8000/
     services.constant('CONSTANTS', {
-        API_HOST: 'http://127.0.0.1:8000/',
-        //API_HOST: 'http://176.36.11.25:8000/',
         API_URL: API_HOST + 'api/',
         PHOTOS_URL: API_HOST + 'static/photos/',
         THUMBNAILS_URL: API_HOST + 'static/thumbnails/'
